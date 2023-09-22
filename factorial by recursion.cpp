f381e6972a927901d9184ccc9d71a38b8e235eb7
PrathamSharma169 <prathamsharma0916@gmail.com>
2023-09-22 22:26:11 +0530

    code to find factorial of any no.

---
+++ factorial by recursion.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int fact(int n){
+    if (n==1){
+        return 1;
+    }
+    else{
+        return n*fact(n-1);
+    }
+}
+int main() {
+    int n;
+    cout<<"what factorial you want?"<<endl;
+    cin>>n;
+    cout<<fact(n);
+
+    return 0;
+}
